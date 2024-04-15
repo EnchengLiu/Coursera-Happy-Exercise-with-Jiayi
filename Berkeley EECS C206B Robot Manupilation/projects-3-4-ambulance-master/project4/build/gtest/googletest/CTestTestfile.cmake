3f4de708b42731a4580039bbc92929b191111c6d
EnchengLiu <enchengliu@berkeley.edu>
2024-04-14 19:11:43 -0700

    Updating 206B lab repo

--- Berkeley EECS C206B Robot Manupilation/projects-3-4-ambulance-master/project4/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/cc/ee106b/sp24/class/ee106b-abh/EECS-C206B/projects-3-4-ambulance/project4/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
